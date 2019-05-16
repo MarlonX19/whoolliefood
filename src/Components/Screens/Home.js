589fba16a3c12b557405c035cf7f3ed0bdcc1886
marlonx19 <marlon.englemam@gmail.com>
2019-05-15 22:05:58 -0300

    Home Screen made class based again

--- src/Components/Screens/Home.js
+++ src/Components/Screens/Home.js
@@ -1,58 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, View, Image, TouchableOpacity, StatusBar } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { ringBell } from '../../Config/Functions';
 
-const Home = () => {
-    return (
-        <View style={styles.container}>
-            <StatusBar
-                hidden
-            />
-            <View>
-                <TouchableOpacity
-                    onPress={() => Actions.Categories()}
-                >
-                    <Image style={styles.imgMenu} source={require('../imgs/menu.png')} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => Actions.OpenBill()}
-                >
-                    <Image style={styles.imgMenu} source={require('../imgs/purse.png')} />
-                </TouchableOpacity>
+class Home extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar
+                    hidden
+                />
+                <View>
+                    <TouchableOpacity
+                        onPress={() => Actions.Categories()}
+                    >
+                        <Image style={styles.imgMenu} source={require('../imgs/menu.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => Actions.OpenBill()}
+                    >
+                        <Image style={styles.imgMenu} source={require('../imgs/purse.png')} />
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        onPress={() => Actions.Cart()}
+                    >
+                        <Image style={styles.imgMenu} source={require('../imgs/cart.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => ringBell()} //Yet to be implemented
+                    >
+                        <Image style={styles.imgMenu} source={require('../imgs/bellIcon.png')} />
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View>
-                <TouchableOpacity
-                    onPress={() => Actions.Cart()}
-                >
-                    <Image style={styles.imgMenu} source={require('../imgs/cart.png')} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => ringBell()} //Yet to be implemented
-                >
-                    <Image style={styles.imgMenu} source={require('../imgs/bellIcon.png')} />
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+        );
+    }
 }
 
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const styles = StyleSheet.create({
